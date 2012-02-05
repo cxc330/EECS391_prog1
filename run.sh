0118cb33fb9bf4a353af36be754b00d42645f7fe
Chris <Chris@192.168.2.1>
2012-02-05 17:16:06 -0500

    Changed README a bit. Modified run.sh to include the firstAgent file and also added compile code so it compiles the agent every time it's run. The agent should now gather gold until there is no more.

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-java -cp lib/SimpleRTSv3.1.jar edu.cwru.SimpleRTS.Main --config data/midasConfig.xml data/rc_3m5t.xml --agent edu.cwru.SimpleRTS.agent.firstAgent 0 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true
+javac -cp 'lib/SimpleRTSv3.1.jar' src/edu/cwru/SimpleRTS/agent/firstAgent.java
+
+java -cp lib/SimpleRTSv3.1.jar:src edu.cwru.SimpleRTS.Main --config data/midasConfig.xml data/rc_3m5t.xml --agent  edu.cwru.SimpleRTS.agent.firstAgent 0 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true
--- src/edu/cwru/SimpleRTS/agent/firstAgent.java
+++ src/edu/cwru/SimpleRTS/agent/firstAgent.java
@@ -1,20 +1,21 @@
 package edu.cwru.SimpleRTS.agent;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import edu.cwru.SimpleRTS.action.Action;
+import edu.cwru.SimpleRTS.action.*;
 import edu.cwru.SimpleRTS.environment.State.StateView;
+import edu.cwru.SimpleRTS.model.resource.ResourceNode.Type;
+import edu.cwru.SimpleRTS.model.resource.ResourceType;
 import edu.cwru.SimpleRTS.model.unit.Unit.UnitView;
 
 public class firstAgent extends Agent {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	static int minGoldToCarry = 0;
+	static String townHall = "TownHall";
+	static String peasant = "Peasant";
 
-	protected firstAgent(int playernum) {
+	public firstAgent(int playernum) {
 		super(playernum);
 		// TODO Auto-generated constructor stub
 	}
@@ -22,7 +23,7 @@ public class firstAgent extends Agent {
 	@Override
 	public Map<Integer, Action> initialStep(StateView state) {
 		// TODO Auto-generated method stub
-		return null;
+		return middleStep(state);
 	}
 
 	@Override
@@ -30,25 +31,74 @@ public class firstAgent extends Agent {
 		// TODO Auto-generated method stub
 		
 		List<Integer> allUnitIds = state.getAllUnitIds();
+		List<Integer> townHalls = findUnitType(allUnitIds, state, townHall);
+		List<Integer> peasants = findUnitType(allUnitIds, state, peasant);
 		
-		for (int x = 0; x < allUnitIds.size(); x++)
+		Map<Integer, Action> actions = new HashMap<Integer,Action>();
+		
+		if(peasants.size() > 0 && townHalls.size() > 0)
 		{
-			Integer unitId = allUnitIds.get(x);
-			UnitView unit = state.getUnit(unitId);
-			
-			if(unit.getTemplateView().canBuild())
-			{
-				Action.createPrimitiveProduction(unitId, unit.getTemplateView().getID());
-			}
+			collectGold(peasants, actions, state, townHalls.get(0));
+		}
+		else
+		{
+			System.out.println("Error: bad config, not enough peasants or town halls to start");
 		}
 		
-		return null;
+		return actions;
 	}
 
 	@Override
 	public void terminalStep(StateView state) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public List<Integer> findUnitType(List<Integer> ids, StateView state, String name)	{
+		
+		List<Integer> unitIds = new ArrayList<Integer>();
+		
+		for (int x = 0; x < ids.size(); x++)
+		{
+			Integer unitId = ids.get(x);
+			UnitView unit = state.getUnit(unitId);
+			
+			if(unit.getTemplateView().getUnitName().equals(name))
+			{
+				unitIds.add(unitId);
+			}
+		}
+		
+		return unitIds;
+	}
+	
+	public void collectGold(List<Integer> peasants, Map<Integer, Action> actionList, StateView state, Integer townHall)	{
+		
+		Action action = null;
+		
+		for (Integer peasantId: peasants)
+		{
+			List<Integer> resourceIds = state.getResourceNodeIds(Type.GOLD_MINE);
+			
+			if(state.getUnit(peasantId).getCargoType() == ResourceType.GOLD && state.getUnit(peasantId).getCargoAmount() > minGoldToCarry)
+			{
+				action = new TargetedAction(peasantId, ActionType.COMPOUNDDEPOSIT, townHall);
+			}
+			else if(resourceIds.size() > 0)
+			{
+				action = new TargetedAction(peasantId, ActionType.COMPOUNDGATHER, resourceIds.get(0));
+			}
+			else
+			{
+				System.out.println("Can't collect anymore gold");
+				//do nothing
+			}
+			
+			if (action != null)
+			{
+				actionList.put(peasantId, action);
+			}
+		}
 	}
 
 }
