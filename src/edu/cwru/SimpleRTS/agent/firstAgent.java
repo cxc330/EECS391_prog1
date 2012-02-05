d03912722aa6ff5761dc9afae41d534dccb37ce5
Chris <Chris@192.168.2.1>
2012-02-05 15:17:26 -0500

    Just added some code to the agent. Trying to run it.

--- src/edu/cwru/SimpleRTS/agent/firstAgent.java
+++ src/edu/cwru/SimpleRTS/agent/firstAgent.java
@@ -1,11 +1,18 @@
 package edu.cwru.SimpleRTS.agent;
 
+import java.util.List;
 import java.util.Map;
 
 import edu.cwru.SimpleRTS.action.Action;
 import edu.cwru.SimpleRTS.environment.State.StateView;
+import edu.cwru.SimpleRTS.model.unit.Unit.UnitView;
 
 public class firstAgent extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	protected firstAgent(int playernum) {
 		super(playernum);
@@ -13,19 +20,33 @@ public class firstAgent extends Agent {
 	}
 
 	@Override
-	public Map<Integer, Action> initialStep(StateView arg0) {
+	public Map<Integer, Action> initialStep(StateView state) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Map<Integer, Action> middleStep(StateView arg0) {
+	public Map<Integer, Action> middleStep(StateView state) {
 		// TODO Auto-generated method stub
+		
+		List<Integer> allUnitIds = state.getAllUnitIds();
+		
+		for (int x = 0; x < allUnitIds.size(); x++)
+		{
+			Integer unitId = allUnitIds.get(x);
+			UnitView unit = state.getUnit(unitId);
+			
+			if(unit.getTemplateView().canBuild())
+			{
+				Action.createPrimitiveProduction(unitId, unit.getTemplateView().getID());
+			}
+		}
+		
 		return null;
 	}
 
 	@Override
-	public void terminalStep(StateView arg0) {
+	public void terminalStep(StateView state) {
 		// TODO Auto-generated method stub
 
 	}
