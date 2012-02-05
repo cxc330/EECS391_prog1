ccc05943cd4815eac172120959ad34ed84e34d92
Chris <Chris@192.168.2.1>
2012-02-05 14:00:50 -0500

    Added the firstAgent class and a src folder

---
+++ src/edu/cwru/SimpleRTS/agent/firstAgent.java
@@ -0,0 +1,33 @@
+package edu.cwru.SimpleRTS.agent;
+
+import java.util.Map;
+
+import edu.cwru.SimpleRTS.action.Action;
+import edu.cwru.SimpleRTS.environment.State.StateView;
+
+public class firstAgent extends Agent {
+
+	protected firstAgent(int playernum) {
+		super(playernum);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Map<Integer, Action> initialStep(StateView arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Integer, Action> middleStep(StateView arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminalStep(StateView arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
